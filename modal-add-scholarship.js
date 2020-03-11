a34d32217142c3aabcb21755444c6f0a2289ef73
Marcelo Kenzo <m.kenzo.takamori@accenture.com>
2020-03-11 17:48:56 -0300

    3

--- modal-add-scholarship.js
+++ modal-add-scholarship.js
@@ -1,3 +1,18 @@
 app.controller('ModalAddScholarship', function($scope, items){
+    $scope.items = items;
     
+    // Change selection on click
+    $scope.changeSelection = function(item) {
+        item.selected = !item.selected;
+    }
+
+    // Button to save selected scholarships
+    $scope.ok = function() {
+
+    }
+
+    // Button to close modal
+    $scope.cancel = function() {
+
+    }
 })
--- myAccount.js
+++ myAccount.js
@@ -1,5 +1,5 @@
 app.controller('myAccount', function($scope, $http, $modal) {
-    $scope.scholarships = {}
+    $scope.scholarships = [];
     $scope.openModal = function() {
         var modalInstance = $modal.open({
             animation: true,
@@ -10,13 +10,35 @@ app.controller('myAccount', function($scope, $http, $modal) {
                 items: $scope.scholarships
             }
         })
-    }
-    function init() {
-        $http.get('https://testapi.io/api/redealumni/scholarships').then(function(response){
-            $scope.scholarships = response;
-        }, function(error){
-            console.log('Ooops! Something went wrong.')
+        modalInstance.result.then(function(){
+            localStorage.setItem('selectedScholarships', JSON.stringify(response));
+        }, function(){
+
         })
+    }
+
+    function getScholarships() {
+        var selection = JSON.parse(localStorage.getItem('b2bUnits')) || [];
+        // checking if the scholarships are saved on browser
+        if (selection.length > 0) {
+            $scope.scholarships = selection;
+        } else {
+            // if not, call http GET
+            $http.get('db.json').then(function(response){
+                $scope.scholarships = response.data;
+                console.log('Scholarships: ', $scope.scholarships)
+                angular.forEach($scope.scholarships, function(item){
+                    item.selected = false;
+                })
+            }, function(error){
+                // TODO: add treatment in case of service going wrong
+                console.log('Ooops! Something went wrong.')
+            })
+        }
+    }
+
+    function init() {
+        getScholarships();
     }
     init();
 });
