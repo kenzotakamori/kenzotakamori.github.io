298f62892e2d79673f63625ab4bc9f3b50e235b8
Marcelo Kenzo <m.kenzo.takamori@accenture.com>
2020-03-16 17:18:58 -0300

    Star Rating change

--- main.js
+++ main.js
@@ -14,6 +14,25 @@ app.directive("starRating", function() {
       scope: {
         rate: '@'
       },
-      templateUrl: "directives/star-rating.html"
+      templateUrl: "directives/star-rating.html",
+      controller: function($scope) {
+        function validateRate() {
+          var arr = []
+          var n = Math.floor(rate)
+          var dec = rate - n
+          for (var i = 0; i < 5; i++){
+            if (i < n) {
+              arr.push('fas fa-star')
+            } else if (dec >= 0.5) {
+              arr.push('fas fa-star-half-alt')
+              dec = 0
+            } else {
+                arr.push('far fa-star')
+            }       
+          }
+          return arr
+        }
+        $scope.stars = validateRate($scope.rate);
+      },
     };
 });
